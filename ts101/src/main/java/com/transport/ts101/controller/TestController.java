5fa7c0b75bbbdf7915cc41ab3119cfc33fe638d3
“srvcool9” <sauravgawli696@gmail.com>
2023-04-03 18:02:27 +0530

    Logger Implemented

--- ts101/src/main/java/com/transport/ts101/controller/TestController.java
+++ ts101/src/main/java/com/transport/ts101/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.transport.ts101.controller;
 
 import com.transport.ts101.repository.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +18,16 @@ import java.util.List;
 @RequestMapping
 public class TestController {
 
+    private static final Logger LOGGER = LogManager.getLogger(TestController.class);
     @Autowired
     private DriveHistoryRepository repository;
 
     @GetMapping(value={"/GetData"})
     public ResponseEntity<List<?>> getData() throws Exception {
         try {
+            LOGGER.info("Fetching Data.......");
             List<?> res = repository.findAll();
+            LOGGER.info("Data fetched successfully.......");
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
